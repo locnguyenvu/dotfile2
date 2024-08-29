4a0fb18badab0ffa8a0c85dd74b717ca70cc1daa
locnguyenvu <ngynvuloc@gmail.com>
2024-08-29 13:32:03 +0700

    [nvim] config for nvim-cmp

--- nvim/config.lua
+++ nvim/config.lua
@@ -68,9 +68,32 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- Language server
-require'cmp'.setup {
+require('lspsaga').setup({
+  lightbulb = {
+    enable = false
+  }
+})
+
+local cmp = require'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
   sources = {
-    { name = 'nvim_lsp' }
+    { name = 'nvim_lsp' },
   }
 }
 
