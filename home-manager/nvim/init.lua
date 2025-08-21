20d7a447aec89d2b7dec8bedbb6dad713665b2e5
locnguyenvu <ngynvuloc@gmail.com>
2025-08-21 21:29:13 +0700

    Update neovim config compatible with neovim v0.11

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -1,5 +1,6 @@
--- vim: ts=2 sw=2 autoindent expandtab
+-- vim: ts=2 sw=2 autoindent expandtab foldmethod=marker
 
+-- lualine {{{
 require('lualine').setup {
   sections = {
     lualine_b = {{'lsp_status'}},
@@ -10,7 +11,9 @@ require('lualine').setup {
     lualine_c = {{'filename', path = 1}}
   }
 }
+-- }}}
 
+-- nvim-cursorline {{{
 require('nvim-cursorline').setup {
   cursorline = {
     enable = true,
@@ -23,7 +26,9 @@ require('nvim-cursorline').setup {
     hl = { underline = true },
   }
 }
+-- }}}
 
+-- bufferline {{{
 vim.opt.termguicolors = true
 require("bufferline").setup {
   options = {
@@ -41,14 +46,16 @@ require("bufferline").setup {
     }
   }
 }
+-- }}}
 
+-- nvim-tree {{{
 require("nvim-tree").setup {
   auto_reload_on_write = false,
   view = {
     relativenumber = true, 
     width = 50,
     float = {
-      enable = true,
+      enable = false,
       quit_on_focus_loss = true,
       open_win_config = {
         width = 50
@@ -63,34 +70,9 @@ require("nvim-tree").setup {
     ignore = true,
   }
 }
+-- }}}
 
--- Indent blank-line
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
-local ibl_highlight = {
-  "RainbowRed",
-  "RainbowYellow",
-  "RainbowBlue",
-  "RainbowOrange",
-  "RainbowGreen",
-  "RainbowViolet",
-  "RainbowCyan",
-}
-local hooks = require "ibl.hooks"
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-
-require("ibl").setup { indent = { highlight = ibl_highlight } }
-
--- Treesitter
+-- Treesitter {{{
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
@@ -99,8 +81,9 @@ require'nvim-treesitter.configs'.setup {
   incremental_selection = { enable = true },
   textobjects = { enable = true },
 }
+-- }}}
 
--- Telescope
+-- Telescope {{{
 require('telescope').setup({
   defaults = {
     layout_config = {
@@ -108,16 +91,33 @@ require('telescope').setup({
     },
   },
 })
+-- }}}
 
--- Flash.nvim
+-- Flash.nvim {{{
 vim.keymap.set({ 'n', 'x', 'o' }, 's', function() require('flash').jump() end)
 vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end)
 vim.keymap.set({'o'}, 'r', function() require('flash').remote() end)
 vim.keymap.set({ 'o', 'x' }, 'R', function() require('flash').treesitter_search() end)
 vim.keymap.set({ 'c' }, '<c-s>', function() require('flash').toggle() end)
+-- }}}
 
+-- Kitty-scrollback
+require('kitty-scrollback').setup()
 
--- Language server
+-- Nvim LSP config
+---- navbuddy {{{
+local navbuddy = require('nvim-navbuddy')
+navbuddy.setup{
+  window = {
+    size = { height = '80%', width = '70%' },
+  },
+  lsp = {
+    auto_attach = true,
+    preference = {'pylsp'}
+  }
+}
+-- }}}
+---- Lspsaga {{{
 require('lspsaga').setup({
   lightbulb = {
     enable = false
@@ -127,7 +127,8 @@ require('lspsaga').setup({
   }
 })
 vim.keymap.set({'n','t'}, '<F12>', '<cmd>Lspsaga term_toggle<CR>')
-
+-- }}}
+---- CMP {{{
 local cmp = require'cmp'
 cmp.setup {
   snippet = {
@@ -150,60 +151,50 @@ cmp.setup {
     { name = 'nvim_lsp' },
   }
 }
-
-
--- Kitty-scrollback
-require('kitty-scrollback').setup()
-
--- Nvim LSP config
---
----- navbuddy
-local navbuddy = require('nvim-navbuddy')
-navbuddy.setup{
-  window = {
-    size = { height = '80%', width = '70%' },
-  },
-  lsp = {
-    auto_attach = true,
-    preference = {'pylsp'}
-  }
-}
-
+-- }}}
+---- keymap {{{
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>w', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-local on_attach = function(client, bufnr)
-  navbuddy.attach(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.lsp.config('*', {
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-  -- Mappings.
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gi', '<cmd>Lspsaga finder imp<CR>', bufopts)
-  vim.keymap.set('n', 'gr', '<cmd>Lspsaga finder ref<CR>', bufopts)
-  vim.keymap.set('n', 'K', '<cmd>Lspsaga peek_definition<CR>', bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', '<cmd>Lspsaga peek_type_definition<CR>', bufopts)
-  vim.keymap.set('n', '<space>rn', '<cmd>Lspsaga rename<CR>', bufopts)
-  vim.keymap.set('n', '<space>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
-end
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    if client.supports_method and client:supports_method("textDocument/documentSymbol") then
+      navbuddy.attach(client, args.buf)
+    end
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(args.buf, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
--- LSP Python setup
+    -- Mappings.
+    local bufopts = { noremap=true, silent=true, buffer=args.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gi', '<cmd>Lspsaga finder imp<CR>', bufopts)
+    vim.keymap.set('n', 'gr', '<cmd>Lspsaga finder ref<CR>', bufopts)
+    vim.keymap.set('n', 'K', '<cmd>Lspsaga peek_definition<CR>', bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', '<cmd>Lspsaga peek_type_definition<CR>', bufopts)
+    vim.keymap.set('n', '<space>rn', '<cmd>Lspsaga rename<CR>', bufopts)
+    vim.keymap.set('n', '<space>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
+  end
+})
+--- }}}
+-- LSP Python {{{
+---- pylsp {{{
 if vim.fn.executable('pylsp') == 1 then
-  require'lspconfig'.pylsp.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
+  vim.lsp.config('pylsp', {
     settings = {
       pylsp = {
         plugins = {
@@ -214,30 +205,41 @@ if vim.fn.executable('pylsp') == 1 then
         }
       }
     }
-  }
+  })
+  vim.lsp.enable('pylsp')
 end
+-- }}}
+---- ruff {{{
 if vim.fn.executable('ruff') == 1 then
-  require'lspconfig'.ruff.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
+  vim.lsp.config('ruff', {
+    capabilities = vim.tbl_deep_extend(
+      'force',
+      require('cmp_nvim_lsp').default_capabilities(
+        vim.lsp.protocol.make_client_capabilities()
+      ),
+      { general = { positionEncodings = { 'utf-16' } } }
+    ),
     init_options = {
       settings = {
         -- Server settings should go here
         builtins = {"ic", "snoop", "pp"}
       }
     }
-  }
+  })
+  vim.lsp.enable('ruff')
 end
-
--- LSP Go setup
+-- }}}
+-- }}}
+-- LSP Go {{{
+----- gopls {{{
 if vim.fn.executable('go') == 1 then
-  require'go'.setup{}
+  vim.lsp.enable('go')
 end
 if vim.fn.executable('gopls') == 1 then
-  require"lspconfig".gopls.setup({
-    on_attach = on_attach,
-  })
+  vim.lsp.enable('gopls')
 end
+-- }}}
+---- auto lint {{{
 if vim.fn.executable('goimports') == 1 then
   local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
   vim.api.nvim_create_autocmd("BufWritePre", {
@@ -248,13 +250,24 @@ if vim.fn.executable('goimports') == 1 then
     group = format_sync_grp,
   })
 end
-
---- LSP ruby-lsp
+-- }}}
+-- }}}
+--- LSP ruby {{{
 if vim.fn.executable('ruby-lsp') == 1 then
   vim.lsp.enable('ruby_lsp')
 end
+-- }}}
+--- LSP vue {{{
+vim.lsp.config('vue_ls', {
+  cmd = { "npx", "vue-language-server", "--stdio" },
+})
+vim.lsp.config('vtsls', {
+  cmd = { "npx", "vtsls", "--stdio" },
+})
+-- }}}
 
---- custom fordmethod
+-- Custom display
+---- folding {{{
 local middot = '·'
 local raquo = '»'
 local small_l = 'ℓ'
@@ -278,8 +291,35 @@ _G.foldtext = function()
 end
 
 vim.opt.foldtext = 'v:lua.foldtext()'
+-- }}}
+---- indent {{{
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+local ibl_highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+require("ibl").setup { indent = { highlight = ibl_highlight } }
+-- }}}
 
--- Fugitive extends commands
+-- Git
+-- Fugitive {{{
 -- fetch and merge current active branch only
 vim.api.nvim_create_user_command('Gfm', function()
   local handle = io.popen("git branch --show-current")
@@ -294,3 +334,4 @@ vim.api.nvim_create_user_command('Gfm', function()
 end, {
   desc = 'Git fetch and merge current branch from origin'
 })
+-- }}}
