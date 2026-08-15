800b22b827f756ee240c0e10d81abf11ce66a86f
locnguyenvu <ngynvuloc@gmail.com>
2026-08-15 22:58:00 +0000

    add toggleterm and dadbod

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,23 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  lazy = false,
+  config = true,
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 25
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    direction = "horizontal",
+    start_in_insert = true,
+    persist_mode = false
+  },
+  keys = {
+    {'<F12>', '<cmd>:ToggleTerm<cr>' },
+    {'<F12>', '<C-\\><C-N><cmd>:ToggleTerm<cr>', mode={'t'}},
+    {'<C-W>', '<C-\\><C-N><C-W>', mode={'t'}},
+  }
+}
