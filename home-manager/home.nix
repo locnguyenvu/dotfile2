6eb4a7d8bb2cf36b80b96cfeeb786a2058830ee5
locnguyenvu <ngynvuloc@gmail.com>
2026-08-19 08:26:30 +0700

    clean up home-manager config, move tmux config outside home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,12 +1,4 @@
 { config, lib, pkgs, ... }:
-let
-  exVimPlugin = { user, repo, rev ? "main" }: pkgs.vimUtils.buildVimPlugin {
-    pname = "${repo}";
-    version = rev;
-    src = builtins.fetchGit { url = "https://github.com/${user}/${repo}.git"; ref = rev; };
-  };
-in
-with builtins;
 {
   home.username = "#username";
   home.homeDirectory = "#homedir";
@@ -20,11 +12,15 @@ with builtins;
     pkgs.fd
     pkgs.fx
     pkgs.httpie
+    pkgs.jq
+    pkgs.neovim
     pkgs.nerd-fonts.daddy-time-mono
-    pkgs.procs
     pkgs.process-compose
+    pkgs.procs
     pkgs.ripgrep
     pkgs.skim
+    pkgs.tree-sitter
+    pkgs.tmux
     pkgs.yazi
     pkgs.yq-go
     pkgs.zoxide
@@ -63,6 +59,7 @@ with builtins;
     };
     profileExtra = ''
     path=(
+      $HOME/.nix-profile/bin
       $HOME/.local/bin
       $path
     )
@@ -111,70 +108,6 @@ with builtins;
     enableZshIntegration = true;
     historyWidget.command = "";
   };
-  programs.tmux = {
-    baseIndex = 1;
-    enable = true;
-    keyMode = "vi";
-    mouse = true;
-    prefix = "C-b";
-    tmuxinator = {
-      enable = true;
-    };
-    extraConfig = 
-      ''
-      bind-key -T copy-mode-vi 'v' send -X begin-selection
-      bind-key -T copy-mode-vi 'y' send -X copy-selection
-      bind-key -T copy-mode-vi 'y' send-keys -X copy-pipe-and-cancel “reattach-to-user-namespace pbcopy”
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
-      bind-key c new-window -c "#{pane_current_path}"
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-
-      bind o if-shell -F '#{window_zoomed_flag}' 'select-pane; resize-pane -Z' 'select-pane'
-      bind \; if-shell -F '#{window_zoomed_flag}' 'select-pane -l; resize-pane -Z' 'select-pane -l'
-
-      set -g default-command $SHELL
-      set -g extended-keys on
-      set -g extended-keys-format csi-u
-      '';
-    plugins = with pkgs; [
-      tmuxPlugins.cpu
-      tmuxPlugins.tmux-fzf
-      tmuxPlugins.open
-      tmuxPlugins.better-mouse-mode
-      tmuxPlugins.tmux-thumbs
-      {
-        plugin = tmuxPlugins.jump;
-        extraConfig = ''
-          set -g @jump-key 's'
-        '';
-      }
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor 'frappe'
-          set -g @catppuccin_window_status_style "rounded"
-          set -g status-left ""
-          set -g @catppuccin_window_text " #W"
-          set -g @catppuccin_window_current_text " #W"
-          set -g @catppuccin_pane_default_text "##{b:pane_current_command}"
-          set -g @catppuccin_window_flags "icon"
-          set -g status-right "#{E:@catppuccin_status_directory}#{E:@catppuccin_status_session}"
-        '';
-      }
-      {
-        plugin = tmuxPlugins.tmux-floax;
-        extraConfig = ''
-          set -g @floax-bind 'f'
-          set -g @floax-bind-menu 'g'
-          set -g @floax-text-color 'white'
-        '';
-      }
-    ];
-  };
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
@@ -186,48 +119,6 @@ with builtins;
   };
   programs.bat = {
     enable = true;
-  };
-  programs.neovim = {
-    defaultEditor = true;
-    enable = true;
-    withPython3 = true;
-    withNodeJs = true;
-    extraConfig = builtins.readFile nvim/vimrc;
-    initLua = builtins.readFile nvim/init.lua;
-    extraWrapperArgs = [
-        "--set" "BUNDLE_DISABLE_SHARED_GEMS" "true"
-        "--unset" "GEM_HOME"
-    ];
-    plugins = with pkgs.vimPlugins; [
-      bufferline-nvim
-      cmp-nvim-lsp
-      cmp-nvim-lua
-      flash-nvim
-      hologram-nvim
-      indent-blankline-nvim
-      lspsaga-nvim
-      lualine-nvim
-      nvim-dap
-      nvim-dap-ui
-      nvim-cmp
-      nvim-cursorline
-      nvim-navbuddy
-      nvim-lspconfig
-      nvim-tree-lua
-      nvim-treesitter
-      nvim-treesitter.withAllGrammars
-      nvim-web-devicons
-      telescope-nvim
-      toggleterm-nvim
-      catppuccin-nvim
-      vim-commentary
-      vim-dadbod-ui
-      vim-dadbod-completion
-      vim-fugitive
-      vim-repeat
-      kitty-scrollback-nvim
-      yazi-nvim
-    ];
   };
   programs.zoxide = {
     enable = true;
